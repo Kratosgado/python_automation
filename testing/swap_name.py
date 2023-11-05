5b77a460567762696cc9f72129bca1c390360808
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2023-11-05 04:15:23 +0000

    fix code not running

--- testing/swap_name.py
+++ testing/swap_name.py
@@ -3,4 +3,6 @@
 def swap_name(name):
     """Swap the first name and the last name of a person's name."""
     name_list = name.split()
+    if(name == ""):
+        return ""
     return name_list[1] + ', ' + name_list[0]
--- testing/swap_name_test.py
+++ testing/swap_name_test.py
@@ -1,15 +1,18 @@
 # file to test swap_name.py
 
 import unittest
-import swap_name
+from swap_name import swap_name
 
 class TestSwapName(unittest.TestCase):
-  
-   def test_swap_name(self):
+   def test_basic(self):
       name = 'John Smith'
       expected = 'Smith, John'
-      self.assertEqual(swap_name.swap_name(name), expected)
-
-if __name__ == '__main__':
-   unittest.main()
+      self.assertEqual(swap_name(name), expected)
    
+   def test_empty(self):
+      name = ""
+      expected = ""
+      self.assertEqual(swap_name(name), expected)
+
+if __name__ == "__main__":
+   unittest.main()
