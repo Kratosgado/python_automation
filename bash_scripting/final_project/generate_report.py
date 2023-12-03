6967aab853907cc1cce599fe84264141ede788cc
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2023-12-03 11:07:42 +0000

    finish generate user report

--- bash_scripting/final_project/generate_report.py
+++ bash_scripting/final_project/generate_report.py
@@ -1,33 +1,30 @@
 import re
 
-def rank_syslog_errors(syslog_file_path):
+def generate_user_report(syslog_file_path):
    # Define a regex pattern for extracting error messages
    error_pattern = r'ERROR (.+)$'
    info_pattern = r'INFO (.+)$'
 
    # Use a dictionary to count the occurrences of each error message
-   error_counts = {}
+   user_report = {}
 
    # Read the syslog file line by line
    with open(syslog_file_path, 'r') as file:
+      user_pattern = r'User  \((\w+)\)$'
       for line in file:
          line = line.strip()
-         error_line = re.search(error_pattern, line)
-         info_line = re.search(info_pattern, line)
-         
-         if(error_line):
-            error_line = line[error_line.start():error_line.end()].split('-')
-            error_message, user = error_line[0], error_line[1]
-            if(error_message in error_counts.keys()):
-               error_counts[error_message] += 1
-            else:
-               error_counts[error_message] = 1
-         if(info_line):
-            user_part = info_line.string.split('-')[1]
-            print(user_part)
-
-   print(error_counts)
+         user_part = re.search(user_pattern, line)
+         if "ticky" not in line or not user_part:
+            continue
+         username = user_part[1]
+         user_report[username] = user_report.get(username, {'infos': 0, 'errors': 0})
+         if "ERROR" in line:
+            user_report[username]['errors'] = user_report[username]['errors'] + 1
+         if "INFO" in line:
+            user_report[username]['infos'] = user_report[username]['infos'] + 1
+   sorted_report = dict(sorted(user_report.items()))
+   print(sorted_report)
 
 # Example usage:
-syslog_path = "/home/kratosgado/projects/python_automation/bash_scripting/log.txt"
-rank_syslog_errors(syslog_path)
+syslog_path = "/home/kratosgado/projects/python/python_automation/bash_scripting/log.txt"
+generate_user_report(syslog_path)
